1ae42515ef6cb1d56427b27f85cb932fcfe00fdc
efficodex <alexandraruizfernandez01@gmail.com>
2025-09-13 06:04:52 -0500

    json created

---
+++ week02/scripts/prophets.js
@@ -0,0 +1,35 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+const cards = document.querySelector('#cards');
+
+
+const getProphetData = async (url) => {
+  
+    const response = await fetch(url); // Wait for the fetch to complete
+    const data = await response.json(); // Wait for the response to be converted to JSON
+    //console.table()(data.prophets); // Log the prophets array to the console
+    displayProphets(data.prophets);
+};
+getProphetData();
+
+const displayProphets = (prophets) => {
+  prophets.forEach((prophet) => {
+    // Create elements to add to the div.cards element
+    let card = document.createElement('section');
+    let fullName = document.createElement('h2');
+    let portrait = document.createElement('img');
+
+    // Build the h2 content out to show the prophet's full name
+    fullName.textContent = `${prophet.name} ${prophet.lastname}`; 
+    // Build the image portrait by setting all the relevant attributes
+    portrait.setAttribute('src', prophet.imageurl);
+    portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`); 
+    portrait.setAttribute('width', '340');
+    portrait.setAttribute('height', '440');
+
+    // Append the section(card) with the created elements
+    card.appendChild(fullName); // append the h2 element to the section(card)
+    card.appendChild(portrait);
+
+    cards.appendChild(card);
+  }); // end of arrow function and forEach loop
+}
